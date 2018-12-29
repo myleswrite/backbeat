6c952aae46bc11ebf7325c9444509a3204606a61
Myles WInstone <myles@writeandweb.uk>
2018-12-29 20:36:42 +0000

    Update backbeat.py
    Removed pandas module (as using csv to open up the csv anyway). Played with using 'with open…' for file access as I read it's better to use that.

--- backbeat.py
+++ backbeat.py
@@ -17,48 +17,44 @@ try:  # For waiting
 except:
 	print("time is required")
 	exit()
-try:  # For saving as CSV
-	import pandas
-except:
-	print("pandas is required")
-	exit()
-try: # For the save as option
+try: # For file dialog
 	from tkinter import filedialog
 	from tkinter import *
 except:
 	print("tkinter.filedialog is required")
 	exit()
+# Create a blank dictionary
+complete = []
 # Open the CSV
 filecsv = Tk()
 filecsv.filename = filedialog.askopenfilename(initialdir = "~/",title = "Select csv file to open",filetypes = (("CSV files","*.csv"),("all files","*.*")))
 print ("Opening file " + filecsv.filename)
-
 # Start work on the CSV file
-worklist = csv.DictReader(open(filecsv.filename, 'r'))
-complete = []
-for row in worklist:
-	print("Working on " + row['Destination'])
-	line = {}
-	try:
-		page = requests.head(row['Destination'],allow_redirects=True,timeout=5)
-		line = dict(
-			Source = row['Source'],
-			Destination = row['Destination'],
-			Status = page.status_code,
-			History = page.history,
-			EndUrl = page.url
-		)
-		complete.append(line)
-	except:
-		print("Url error for " + row['Destination'])
-		line = dict(
-			Source = row['Source'],
-			Destination = row['Destination'],
-			Status = 'ERROR',
-			History = '',
-			EndUrl = ''
-		)
-		complete.append(line)
+with open(filecsv.filename, 'r') as f: # Open the file
+	worklist = csv.DictReader(f)
+	for row in worklist:
+		print("Working on " + row['Destination'])
+		line = {}
+		try:
+			page = requests.head(row['Destination'],allow_redirects=True,timeout=5)
+			line = dict(
+				Source = row['Source'],
+				Destination = row['Destination'],
+				Status = page.status_code,
+				History = page.history,
+				EndUrl = page.url
+			)
+			complete.append(line)
+		except:
+			print("Url error for " + row['Destination'])
+			line = dict(
+				Source = row['Source'],
+				Destination = row['Destination'],
+				Status = 'ERROR',
+				History = '',
+				EndUrl = ''
+			)
+			complete.append(line)
 print(complete)
 # See if we want to save the file
 dosave = input("Do you want to save a CSV — Type 'Y' for yes or anything else for no. ")
@@ -66,8 +62,13 @@ if dosave == 'Y' or dosave =='y': #If we're saving let's save
 	file_name = filedialog.asksaveasfilename()
 	#Attempt to create a CSV
 	csvname = file_name + '.csv'
-	pandas.DataFrame(complete).to_csv(csvname, index=False)
-	print("Saved CSV")
+	fieldnames = ['Source','Destination','Status','History','EndUrl']
+	with open(csvname,'w') as s: #Save the file
+		w = csv.DictWriter(s, fieldnames = fieldnames)
+		w.writeheader()
+		for k in complete:
+			w.writerow(k)
+		print("Saved CSV")
 else:
 	print("Okay we're done here.")
 # Here endeth the lessen
